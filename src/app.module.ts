0d316b6cccfcf4090095e8ea0b2aade3a33202fd
evidence-codes <adejuwonevidence181@gmail.com>
2025-01-07 02:39:59 +0100

    feat: create mongodb connection and register modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DatabaseModule } from './infrastructure/database/database.module';
+import { UserController } from './infrastructure/controllers/user.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:tHN9vpHzKPObDWl2@streamify-cluster.owtsp.mongodb.net/?retryWrites=true&w=majority&appName=streamify-cluster',
+    ),
+    DatabaseModule,
+  ],
+  controllers: [UserController],
 })
 export class AppModule {}
